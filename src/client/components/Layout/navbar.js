5efc93cbce2585b7373e41614dc61e4cb26230af
Dean Slama Jr <deanslamajr@gmail.com>
2016-10-31 15:15:22 -0500

    made menu collapse when an item is clicked

--- src/client/components/Layout/navbar.js
+++ src/client/components/Layout/navbar.js
@@ -1,27 +1,53 @@
 import React from 'react';
 import { Link } from 'react-router';
 import cssModules from 'react-css-modules';
+import classNames from 'classnames';
 
 import styles from './navbar.css';
 
 class NavBar extends React.Component {
+  constructor() {
+    super();
+
+    this._onMenuClick = this._onMenuClick.bind(this);
+    this._onLinkClick = this._onLinkClick.bind(this);
+
+    this.state = {
+      expanded: false
+    };
+  }
+
+  _onMenuClick() {
+    this.setState({ expanded: !this.state.expanded });
+  }
+
+  _onLinkClick() {
+    this.setState({ expanded: false })
+  }
+
   render() {
+    const conditionalShow = classNames({
+      [styles['show']]: this.state.expanded
+    });
+
     return (
       <div styleName='outer-container'>
         <span styleName='middle-container'>
           <span>Dean H. Slama Jr.</span>
-          <span styleName='mobile'>Menu</span>
+          <span onClick={this._onMenuClick} styleName='mobile'>Menu</span>
         </span>
-        <span styleName='inner-container'>
-          <span styleName='link'>Posts</span>
-          <span styleName='link'>
-            <Link to="/about">Projects</Link>
+        <span styleName='inner-container' className={conditionalShow}>
+          <span onClick={this._onLinkClick} styleName='link'>
+            <Link to="/posts">Posts</Link>
           </span>
-          <span styleName='link'>
+          <span onClick={this._onLinkClick} styleName='link'>
+            <Link to="/projects">Projects</Link>
+          </span>
+          <span onClick={this._onLinkClick} styleName='link'>
             <Link to="/stuff">Stuff</Link>
           </span>
-          <span styleName='link'>
-            <Link to="/blogs">Others</Link>
+          <span onClick={this._onLinkClick} styleName='link'>
+            <Link to="/others">Others</Link>
           </span>
         </span>
       </div>
--- src/client/components/routes.js
+++ src/client/components/routes.js
@@ -2,19 +2,19 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
 import Layout from './Layout';
-import About from './About';
-//import Posts from './Posts';
-//import Post from './Post';
-import Blogs from './Blogs';
+import Posts from './Posts';
+import Projects from './About';
 import Stuff from './Stuff';
+import Others from './Blogs';
 import Login from './Login';
 
 export default (
   <Route path='/' component={Layout}>
-    <IndexRoute component={About} />
-    <Route path='about' component={About} />
-    <Route path='blogs' component={Blogs} />
+    <IndexRoute component={Posts} />
+    <Route path='posts' component={Posts} />
+    <Route path='projects' component={Projects} />
     <Route path='stuff' component={Stuff} />
+    <Route path='others' component={Others} />
     <Route path='babylou' component={Login} />
   </Route>
 );

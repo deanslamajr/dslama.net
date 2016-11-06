5c51be48e365e04d42cab08d5946f96391e3a3bd
Dean Slama Jr <deanslamajr@gmail.com>
2016-11-06 15:39:29 -0600

    fleshed out login page content, added login styles

--- src/client/components/Login/index.js
+++ src/client/components/Login/index.js
@@ -1,12 +1,38 @@
 import React from 'react';
+import cssModules from 'react-css-modules';
 
-export default class Login extends React.Component {
+import styles from './login.css';
+
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: '',
+      password: ''
+    }
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(type, event) {
+    this.setState({[type]: event.target.value});
+  }
+
+  handleSubmit() {
+    console.log('state:', this.state);
+  }
+
   render() {
     return (
-      <div>
-          <input type='text' placeholder='name' />
-          <input type='text' placeholder='password' />
+      <div styleName='container'>
+          <input type='text' onChange={this.handleChange.bind(this, 'username')} placeholder='username' styleName='form-element' />
+          <input type='password' onChange={this.handleChange.bind(this, 'password')} placeholder='password' styleName='form-element' />
+          <button onClick={this.handleSubmit} styleName='form-element'>Login</button>
+          <div styleName='form-element' />
       </div>
     );
   }
 }
+
+export default cssModules(Login, styles);

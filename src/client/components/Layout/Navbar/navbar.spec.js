863ef170423e4194bda143f3601d1b22dbaf28de
Dean H Slama Jr <dslama@luc.id>
2016-12-04 21:35:49 -0600

    Add jest (#7)
    * moved Navbar component files into new directory
    
    * added jest test framework
    
    added snapshot test for Navbar component
    
    * added enzyme test to navbar
    
    * refactored navbar componenet to be composed of container and presentation components
    
    * cleaned up some container/presenter logic
    
    added some more unit tests
    
    * ad more tests to navbar component
    
    cleaned up some component naming

---
+++ src/client/components/Layout/Navbar/navbar.spec.js
@@ -0,0 +1,97 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { shallow, mount } from 'enzyme';
+
+import Navbar from './navbar';
+import NavbarContainer from '.';
+
+const title = 'test title';
+const menuItems = ['first', 'second', 'third'];
+
+describe('Navbar', () => {
+  const expanded = false;
+  const onMenuClick = jest.fn();
+  const onLinkClick = jest.fn();
+  const element = (
+    <Navbar
+      title={title}
+      menuItems={menuItems}
+      expanded={expanded}
+      onMenuClick={onMenuClick}
+      onLinkClick={onLinkClick}
+    />
+  );
+
+  let navbar;
+
+  beforeEach(() => {
+    navbar = shallow(element);
+
+    onMenuClick.mockReset();
+    onLinkClick.mockReset();
+  });
+
+  it('should render', () => {
+    const tree = renderer.create(element).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('should properly handle menu click events', () => {
+    expect(onMenuClick.mock.calls.length).toBe(0);
+
+    navbar.find('.mobile').simulate('click');
+
+    expect(onMenuClick.mock.calls.length).toBe(1);
+  });
+
+  it('should properly handle link click events', () => {
+    expect(onLinkClick.mock.calls.length).toBe(0);
+
+    navbar.find('.inner-container').childAt(0).simulate('click');
+    
+    expect(onLinkClick.mock.calls.length).toBe(1);
+  });
+});
+
+describe('Navbar container', () => {
+  const currentPath = '/currentPath';
+
+  const element = (
+    <NavbarContainer
+      currentPath={currentPath}
+      title={title}
+      menuItems={menuItems}
+    />
+  );
+
+  let navbarContainer;
+
+  beforeEach(() => {
+    navbarContainer = mount(element);
+  });
+
+  it('should render', () => {
+    const tree = renderer.create(element).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('should expand/collaps on click', () => {
+    expect(navbarContainer.find('.show').length).toBe(0);
+
+    navbarContainer.find('.mobile').simulate('click');
+    expect(navbarContainer.find('.show').length).toBe(1);
+
+    navbarContainer.find('.mobile').simulate('click');
+    expect(navbarContainer.find('.show').length).toBe(0);
+  });
+
+  it('should make a clicked item active while inactivating all other items', () => {
+    expect(navbarContainer.find('.inner-container').childAt(0).is('.active')).toBeFalsy();
+
+    navbarContainer.find('.inner-container').childAt(0).simulate('click');
+
+    expect(navbarContainer.find('.inner-container').childAt(0).is('.active')).toBeTruthy();
+  });
+});

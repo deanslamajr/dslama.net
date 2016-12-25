674c7763e854311cdd59a561a2ab6678b7821e06
Dean Slama Jr <deanslamajr@gmail.com>
2016-12-24 18:15:52 -0600

    cleaned up the snippets container logic that chooses whether to display the addSnippet or Snippet component

--- src/client/components/Snippets/index.js
+++ src/client/components/Snippets/index.js
@@ -4,11 +4,8 @@ import SnippetsContainer from './SnippetsContainer';
 
 export default class SnippetsParentContainer extends React.Component {
   render() {
-    // If this is a child path, render that instead of the parent component
-    const { children } = this.props;
+    const { children: addSnippetComponent } = this.props;
 
-    return children
-      ? children
-      : <SnippetsContainer/>
+    return addSnippetComponent || <SnippetsContainer/>
   }
 }

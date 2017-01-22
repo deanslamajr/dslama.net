0535353d9c1c2fbfd03498dddcb110bb82504596
Dean Slama Jr <deanslamajr@gmail.com>
2017-01-22 14:00:55 -0600

    restyled forms to look better on smaller viewports

--- src/client/components/Snippets/Add/Add.js
+++ src/client/components/Snippets/Add/Add.js
@@ -18,7 +18,7 @@ class Add extends React.Component {
       url,
       imagePath } = this.props;
 
-    const serverResultClasses = ['form-element'];
+    const serverResultClasses = ['server-message'];
     if (serverResult.error) {
       serverResultClasses.push('error');
     }
@@ -28,14 +28,14 @@ class Add extends React.Component {
 
     return (
       <form styleName='container' onSubmit={submitHandler}>
-        <div styleName={serverResultClasses.join(' ')}>{serverResult.message}</div>
         <input type='text' value={author} onChange={changeHandler.bind(this, 'author')} placeholder='author' styleName='form-element' />
         <textarea value={title} onChange={changeHandler.bind(this, 'title')} placeholder='title' styleName='form-element' />
         <textarea value={quote} onChange={changeHandler.bind(this, 'quote')} placeholder='quote' styleName='form-element' />        
         <input value={publishDate} type='text' onChange={changeHandler.bind(this, 'publishDate')} placeholder='publishDate' styleName='form-element' />
         <input value={publication} type='text' onChange={changeHandler.bind(this, 'publication')} placeholder='publication' styleName='form-element' />
-        <input value={url} type='text' onChange={changeHandler.bind(this, 'url')} placeholder='url' styleName='form-element' />
+        <textarea value={url} onChange={changeHandler.bind(this, 'url')} placeholder='url' styleName='form-element' />
         { /*<input value={imagePath} type='text' onChange={changeHandler.bind(this, 'imagePath')} placeholder='imagePath' styleName='form-element' /> */ }
+        <div styleName={serverResultClasses.join(' ')}>{serverResult.message}</div>
         <input type='submit' value='Add Snippet' styleName='form-element' />
       </form>
     );

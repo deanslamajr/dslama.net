2bf2e24cb38c9f91b902a7c2f9e896c9eeee30f8
Dean Slama Jr <deanslamajr@gmail.com>
2017-01-29 16:34:06 -0600

    Added sentry error reporting to production build

--- config/webpack/client.base.babel.js
+++ config/webpack/client.base.babel.js
@@ -1,21 +1,8 @@
-import HtmlWebpackPlugin  from 'html-webpack-plugin';
 import ExtractTextPlugin from 'extract-text-webpack-plugin';
-import DefinePlugin from 'webpack/lib/DefinePlugin';
 
 // Post CSS Plugins
 import cssNext from 'postcss-cssnext';
 import cssImport from 'postcss-import';
-
-// App constants
-import constants from '../constants';
-
-// Mapping of build-time replacements for DefinePlugin
-const PICS_DOMAIN = 'PICS_DOMAIN';
-const replacements =  {
-  'process.env': {
-    [PICS_DOMAIN]: JSON.stringify(constants.get(PICS_DOMAIN))
-  }
-};
 
 module.exports = {
   entry: {
@@ -24,7 +11,6 @@ module.exports = {
       './src/client/index.js',
     ],
   },
-  
   module: {
     loaders: [
       {
@@ -41,32 +27,20 @@ module.exports = {
       }
     ]
   },
-
   postcss: function (webpack) {
     return [
       cssImport,
       cssNext
     ];
   },
-
   output: {
     publicPath: '/assets/',
     path: './public/assets',
     filename: '[name].js',
   },
-  
   plugins: [
-    new DefinePlugin(replacements),
-    new HtmlWebpackPlugin({
-      template: __dirname + '/../../src/client/index.ejs',
-      filename: '../index.ejs',
-      inject: 'body',
-      markupTarget: '<%- markup -%>'
-    }),
     new ExtractTextPlugin('[contenthash][name].css', {
       allChunks: true
     })
-  ],
-
-  devtool: 'eval-source-map',
+  ]
 };
---
+++ config/webpack/client.dev.babel.js
@@ -0,0 +1,33 @@
+import merge from 'webpack-merge';
+
+import client from './client.base.babel';
+
+import DefinePlugin from 'webpack/lib/DefinePlugin';
+import HtmlWebpackPlugin  from 'html-webpack-plugin';
+
+// App constants
+import constants from '../constants';
+
+// Mapping of build-time replacements for DefinePlugin
+const PICS_DOMAIN = 'PICS_DOMAIN';
+const replacements =  {
+  'process.env': {
+    [PICS_DOMAIN]: JSON.stringify(constants.get(PICS_DOMAIN)),
+    // JSON.stringify() renders in app as undefined, this is expected behavior
+    sentryDNS: JSON.stringify()
+  }
+};
+
+module.exports = merge(client, {
+  plugins: [
+    new DefinePlugin(replacements),
+    new HtmlWebpackPlugin({
+      template: __dirname + '/../../src/client/index.ejs',
+      filename: '../index.ejs',
+      inject: 'body',
+      markupTarget: '<%- markup -%>',
+      sentryCDN: ''
+    }),
+  ],
+  devtool: 'eval-source-map'
+});

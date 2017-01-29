8c492f456084b49ae4ef239d9c91c40dc2250d26
Dean Slama Jr <deanslamajr@gmail.com>
2017-01-29 16:34:06 -0600

    Added sentry error reporting to production build

--- config/webpack/client.prod.babel.js
+++ config/webpack/client.prod.babel.js
@@ -2,6 +2,7 @@ import merge from 'webpack-merge';
 
 import client from './client.base.babel';
 
+import HtmlWebpackPlugin  from 'html-webpack-plugin';
 import DedupePlugin from 'webpack/lib/optimize/DedupePlugin';
 import UglifyJsPlugin from 'webpack/lib/optimize/UglifyJsPlugin';
 import DefinePlugin from 'webpack/lib/DefinePlugin';
@@ -9,6 +10,16 @@ import CompressionPlugin from 'compression-webpack-plugin';
 
 // App constants
 import constants from '../constants';
+
+// Mapping of build-time replacements for DefinePlugin
+const PICS_DOMAIN = 'PICS_DOMAIN';
+const replacements =  {
+  'process.env': {
+    [PICS_DOMAIN]: JSON.stringify(constants.get(PICS_DOMAIN)),
+    'NODE_ENV': JSON.stringify('production'),
+    sentryDNS: JSON.stringify(constants.get('SENTRY_DNS'))
+  }
+};
 
 module.exports = merge(client, {
   output: {
@@ -19,11 +30,7 @@ module.exports = merge(client, {
   plugins: [
     // needed for react to be put in production mode
     // @see https://facebook.github.io/react/docs/optimizing-performance.html#use-the-production-build
-    new DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify('production')
-      }
-    }),
+    new DefinePlugin(replacements),
     new DedupePlugin(),
     new UglifyJsPlugin({
       compress: {
@@ -36,7 +43,14 @@ module.exports = merge(client, {
       test: /\.js$/,
       threshold: 10240,
       minRatio: 0.8
-    })
+    }),
+    new HtmlWebpackPlugin({
+      template: __dirname + '/../../src/client/index.ejs',
+      filename: '../index.ejs',
+      inject: 'body',
+      markupTarget: '<%- markup -%>',
+      sentryCDN: '<script src="https://cdn.ravenjs.com/3.9.2/raven.min.js"></script>'
+    }),
   ],
   devtool: 'source-map',
 });
--- src/client/index.js
+++ src/client/index.js
@@ -2,6 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
+const sentryDNS = process.env.sentryDNS;
+
+// App bootstrap
 window.onload = () => {
   ReactDOM.render(<App/>, document.getElementById('app-mount'));
 };
+
+// Client error reporting - prod only
+if (window.Raven) {
+  window.Raven.config(sentryDNS).install();
+}

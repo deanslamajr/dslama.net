e712e39d3fd50044a09af2c534d9f1257d3b115b
Dean Slama <deanslamajr@gmail.com>
2018-09-25 20:27:00 -0600

    Loader refactored

--- src/client/components/Loader/index.js
+++ src/client/components/Loader/index.js
@@ -1,11 +1,14 @@
 import React from 'react'
-import cssModules from 'react-css-modules'
+import styled from 'styled-components'
 
-import styles from './loader.css'
+const Container = styled.div`
+  width: 50px;
+  margin: 7rem auto 0;
+`
 
-function loader () {
+function Loader () {
   return (
-    <div styleName='container' title='1'>
+    <Container>
       <svg version='1.1' id='loader-1' xmlns='http://www.w3.org/2000/svg' x='0px' y='0px'
         width='50px' height='50px' viewBox='0 0 50 50'>
         <path fill='#004741' d='M25.251,6.461c-10.318,0-18.683,8.365-18.683,18.683h4.068c0-8.071,6.543-14.615,14.615-14.615V6.461z'>
@@ -18,8 +21,8 @@ function loader () {
             repeatCount='indefinite' />
         </path>
       </svg>
-    </div>
+    </Container>
   )
 }
 
-export default cssModules(loader, styles, { allowMultiple: true })
+export default Loader

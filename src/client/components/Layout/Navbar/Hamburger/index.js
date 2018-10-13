82481b412188356ec3077ac93938b41d8a2e6d25
Dean Slama <deanslamajr@gmail.com>
2018-10-13 13:45:08 -0600

    refactored Hamburger component

--- src/client/components/Layout/Navbar/Hamburger/index.js
+++ src/client/components/Layout/Navbar/Hamburger/index.js
@@ -1,25 +1,52 @@
 import React from 'react'
-import cssModules from 'react-css-modules'
-import classNames from 'classnames'
+import styled from 'styled-components'
 
-import styles from './hamburger.css'
+import { media } from '../../../../style/style-utils'
+
+const OuterContainer = styled.div`
+  display: none;
+  :hover{
+    cursor: pointer;
+  }
+
+  ${media.phoneMax`
+    display: inline;
+    padding-top: .2rem;
+    padding-right: .3rem;
+  `}
+`
+
+const Line = styled.span`
+  width: 35px;
+  height: 3px;
+  background-color: #ecf0f1;
+  display: block;
+  margin: 0 auto 7px;
+  transition: all 0.3s ease-in-out;
+
+  :nth-child(2){
+    opacity: ${props => props.expanded ? 0 : 'inherit'};
+  }
+  :nth-child(1){
+    transform: ${props => props.expanded ? 'translateY(10px) rotate(45deg)' : 'inherit'};
+  }
+  :nth-child(3){
+    transform: ${props => props.expanded ? 'translateY(-10px) rotate(-45deg)' : 'inherit'};
+  }
+`
 
 class Hamburger extends React.Component {
   render () {
     const { onMenuClick, expanded } = this.props
 
-    const isActive = classNames({
-      active: expanded
-    }, 'mobile', 'hamburger')
-
     return (
-      <div onClick={onMenuClick} styleName={isActive}>
-        <span styleName='line' />
-        <span styleName='line' />
-        <span styleName='line' />
-      </div>
+      <OuterContainer onClick={onMenuClick}>
+        <Line expanded={expanded} />
+        <Line expanded={expanded} />
+        <Line expanded={expanded} />
+      </OuterContainer>
     )
   }
 }
 
-export default cssModules(Hamburger, styles, { allowMultiple: true })
+export default Hamburger

511ec1a53600e84d0f28d1c99b3edaaa891d1bba
Dean Slama <deanslamajr@gmail.com>
2018-10-22 12:42:39 -0600

    refactored Add component

--- src/client/components/Readings/Add/Add.js
+++ src/client/components/Readings/Add/Add.js
@@ -1,7 +1,38 @@
 import React from 'react'
-import cssModules from 'react-css-modules'
+import styled from 'styled-components'
 
-import styles from './add.css'
+import { media } from '../../../style/style-utils'
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  margin-top: 1.5rem;
+`
+
+const Input = styled.input`
+  width: 45%;
+  margin: 0.75rem auto;
+
+  ${media.phoneMax`
+    width: 85%;
+    margin: 0.5rem auto;
+  `}
+
+  height: ${props => props.placeholder === 'title' || props.placeholder === 'url' ? '5em' : ''};
+  height: ${props => props.placeholder === 'quote' ? '30em' : ''};
+`
+
+const ServerResponse = styled.div`
+  width: 25%;
+  height: 1em;
+  margin: .25rem auto;
+  color: ${props => props.error ? 'red' : 'green'};
+
+  ${media.phoneMax`
+    width: 65%;
+    height: 2em;
+  `}
+`
 
 class Add extends React.Component {
   render () {
@@ -16,26 +47,49 @@ class Add extends React.Component {
       publication,
       url } = this.props
 
-    const serverResultClasses = ['server-message']
-    if (serverResult.error) {
-      serverResultClasses.push('error')
-    } else {
-      serverResultClasses.push('success')
-    }
-
     return (
-      <form styleName='container' onSubmit={submitHandler}>
-        <input type='text' value={author} onChange={changeHandler.bind(this, 'author')} placeholder='author' styleName='form-element' />
-        <textarea value={title} onChange={changeHandler.bind(this, 'title')} placeholder='title' styleName='form-element' />
-        <textarea value={quote} onChange={changeHandler.bind(this, 'quote')} placeholder='quote' styleName='form-element' />
-        <input value={publishDate} type='text' onChange={changeHandler.bind(this, 'publishDate')} placeholder='publishDate' styleName='form-element' />
-        <input value={publication} type='text' onChange={changeHandler.bind(this, 'publication')} placeholder='publication' styleName='form-element' />
-        <textarea value={url} onChange={changeHandler.bind(this, 'url')} placeholder='url' styleName='form-element' />
-        <div styleName={serverResultClasses.join(' ')}>{serverResult.message}</div>
-        <input type='submit' value='Add Reading' styleName='form-element' />
-      </form>
+      <Form onSubmit={submitHandler}>
+        <Input
+          value={author}
+          type='text'
+          onChange={(e) => changeHandler('author', e)}
+          placeholder='author'
+        />
+        <Input
+          as='textarea'
+          value={title}
+          onChange={(e) => changeHandler('title', e)}
+          placeholder='title'
+        />
+        <Input
+          as='textarea'
+          value={quote}
+          onChange={(e) => changeHandler('quote', e)}
+          placeholder='quote'
+        />
+        <Input
+          value={publishDate}
+          type='text'
+          onChange={(e) => changeHandler('publishDate', e)}
+          placeholder='publishDate'
+        />
+        <Input
+          value={publication}
+          type='text'
+          onChange={(e) => changeHandler('publication', e)}
+          placeholder='publication'
+        />
+        <Input
+          as='textarea'
+          value={url}
+          onChange={(e) => changeHandler('url', e)}
+          placeholder='url'
+        />
+        <ServerResponse error={serverResult.error}>{serverResult.message}</ServerResponse>
+        <Input type='submit' value='Add Reading' />
+      </Form>
     )
   }
 }
 
-export default cssModules(Add, styles, { allowMultiple: true })
+export default Add
